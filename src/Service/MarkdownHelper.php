4250449ba2cc348d1ca3a6800bd57f6ed0c31cea
plam <plam@localhost.dev>
2020-06-17 10:39:22 +0000

    Before installing bundles with average docs

---
+++ src/Service/MarkdownHelper.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Service;
+
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
+
+class MarkdownHelper 
+{
+    /**
+     * @var AdapterInterface
+     */
+    private $cache;
+
+    /**
+     * @var MarkdownParserInterface
+     */
+    private $markdown;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @var bool
+     */
+    private $isDebug;
+
+    public function __construct(AdapterInterface $cache, 
+                                MarkdownParserInterface $markdown, 
+                                LoggerInterface $markdownLogger, 
+                                bool $isDebug)
+    {
+        $this->cache = $cache;
+
+        $this->markdown = $markdown;
+        
+        $this->logger = $markdownLogger;
+
+        $this->isDebug = $isDebug;
+    }
+
+    public function parse(string $source): string
+    {
+        if (stripos($source, 'bacon') !== false) {
+            $this->logger->info('They are talking about bacon again!');
+        }
+
+        if($this->isDebug) {
+            return $this->markdown->transformMarkdown($source);
+        }
+
+        $item = $this->cache->getItem('markdown_'.md5($source));
+        if (!$item->isHit()) {
+            $item->set($this->markdown->transformMarkdown($source));
+            $this->cache->save($item);
+        }
+
+        return $item->get();
+    }
+}
