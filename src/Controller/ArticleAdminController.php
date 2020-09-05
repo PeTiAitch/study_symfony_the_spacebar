98fa6b99efa7a1c3824e06c1add4d44b9efeb020
plam <plam@localhost.dev>
2020-09-05 07:26:23 +0000

    Article Admin & Low-Level Access Controls

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -9,16 +9,28 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
-/**
- * @IsGranted("ROLE_ADMIN_ARTICLE")
- */
+
 class ArticleAdminController extends AbstractController
 {
     /**
+     * @IsGranted("ROLE_ADMIN_ARTICLE")
+     * 
      * @Route("/admin/article/new", name="admin_article_new")
      */
     public function new(EntityManagerInterface $em)
     {
         die('todo');
+    }
+
+    /**
+     * @Route("/admin/article/{id}/edit")
+     */
+    public function edit(Article $article)
+    {
+        if ($article->getAuthor() != $this->getUser() && !$this->isGranted('ROLE_ADMIN_ARTICLE')) {
+            throw $this->createAccessDeniedException('No access!');
+        }
+        
+        dd($article);
     }
 }
