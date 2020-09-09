5e8fdf3bf9eae96bb1c7c269cd80cdf0e68272c9
plam <plam@localhost.dev>
2020-09-09 08:21:20 +0000

    Registration Form

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserRegistrationFormType;
 use App\Security\LoginFormAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -45,15 +46,17 @@ class SecurityController extends AbstractController
                             GuardAuthenticatorHandler $guardHandler, 
                             LoginFormAuthenticator $formAuthenticator)
     {
-        // TODO - use Symfony forms & validation
-        if ($request->isMethod('POST')) {
-            $user = new User();
-            $user->setEmail($request->request->get('email'))
-            ->setFirstName('Mystery')
-            ->setPassword($passwordEncoder->encodePassword(
-                $user,
-                $request->request->get('password')
-            ));
+        $form = $this->createForm(UserRegistrationFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var User $user */
+            $user = $form->getData();
+            $user
+                ->setPassword($passwordEncoder->encodePassword(
+                    $user,
+                    $user->getPassword()
+                ));
 
             $em = $this->getDoctrine()->getManager();
             $em->persist($user);
@@ -67,6 +70,8 @@ class SecurityController extends AbstractController
             );
         }
 
-        return $this->render('security/register.html.twig');
+        return $this->render('security/register.html.twig', [
+            'registrationForm' => $form->createView()
+        ]);
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -36,7 +36,7 @@ class User implements UserInterface
     /**
      * @Groups("main")
      * 
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $firstName;
 
