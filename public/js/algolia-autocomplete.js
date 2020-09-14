f70b8be2821c04e0b8089354a515549414c9e845
plam <plam@localhost.dev>
2020-09-14 09:08:41 +0000

    The buildView() Method

--- public/js/algolia-autocomplete.js
+++ public/js/algolia-autocomplete.js
@@ -16,16 +16,4 @@ $(document).ready(() => {
             }
         ])
     });
-
-
-    // $('.js-user-autocomplete').autocomplete({ hint: false }, [
-    //     {
-    //         source: function (query, cb) {
-    //             cb([
-    //                 { value: 'foo' },
-    //                 { value: 'bar' }
-    //             ])
-    //         }
-    //     }
-    // ]);
 });
