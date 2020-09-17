d0927442290b90abcb89eb603280b122cd37434f
plam <plam@localhost.dev>
2020-09-17 08:58:37 +0000

    Custom Validator

--- src/Form/Model/UserRegistrationFormModel.php
+++ src/Form/Model/UserRegistrationFormModel.php
@@ -3,12 +3,14 @@
 namespace App\Form\Model;
 
 use Symfony\Component\Validator\Constraints as Assert;
+use App\Validator\UniqueUser;
 
 class UserRegistrationFormModel
 {
     /**
      * @Assert\NotBlank(message="Please enter an email")
      * @Assert\Email()
+     * @UniqueUser()
      */
     public $email;
 
